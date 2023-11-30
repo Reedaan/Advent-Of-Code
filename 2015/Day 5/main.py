0783828940ccbed980a67548b58a4d09270b49d0
Reedaan <119610449+Reedaan@users.noreply.github.com>
2023-11-30 07:29:57 +0100

    Add files via upload

--- 2015/Day 5/main.py
+++ 2015/Day 5/main.py
@@ -6,21 +6,55 @@ data = [line.replace(" ", "") for line in data]
 print(data)
 nice_strings = []
 
+#Part one
 #Nice string of letters is a string that:
 #At least 3 vowels (aeiou) [done]
 #At least one letter that appears twice in a row example ->(xx)
 #DOESNT contain the strings "ab" | "cd" | "pq" | "xy" [done]
 
-for line in data:
-    #Check if the string doesnt contain "ab" | "cd" | "pq" | "xy"
-    if "ab" in line or "cd" in line or "pq" in line or "xy" in line:
-        pass
-    else:
-        #Count the vowels in string
-        vowel_count = 0
-        for letter in range(len(line)):
-            if "a" == line[letter] or "e" == line[letter] or "i" == line[letter] or "o" == line[letter] or "u" == line[letter]:
-                vowel_count += 1
-        if vowel_count >= 3:
-            print("valid")
-    # print(line)
+# def part_one():
+#     for line in data:
+#         #Check if the string doesnt contain "ab" | "cd" | "pq" | "xy"
+#         if "ab" in line or "cd" in line or "pq" in line or "xy" in line:
+#             pass
+#         else:
+#             #Count the vowels in string
+#             vowel_count = 0
+#             for letter in range(len(line)):
+#                 if "a" == line[letter] or "e" == line[letter] or "i" == line[letter] or "o" == line[letter] or "u" == line[letter]:
+#                     vowel_count += 1
+#             if vowel_count >= 3:
+#                 #Check if any letter appears twice in a row
+#                 for index, letter in enumerate(line):
+#                     try:
+#                         if line[index + 1] == letter:
+#                             nice_strings.append(line)
+#                             break
+#                     except IndexError:
+#                         pass
+#     return len(nice_strings)
+
+# print(part_one())
+
+#Part two
+#Pair of two letters that appear at least twice (without overlapping)
+#Contains at least one letter, which repeats with exactly one letter between them (xyx, efe) [done]
+
+def part_two():
+    for line in data:
+        repeated = False
+        for index, letter in enumerate(line):
+            #At least one letter which repeats with exactly one between them
+            try:
+                if line[index + 2] == letter:
+                    repeated = True
+            except IndexError:
+                pass
+        if repeated:
+            
+        
+            
+                
+
+
+print(part_two())
---
+++ 2023/Day 1/main.py
@@ -0,0 +1,5 @@
+data = open("/home/odinproject/repos/Advent-Of-Code/2023/Day 1/input.txt", "r").readlines()
+data = [line.strip(" ") for line in data]
+data = [line.strip("\n") for line in data]
+data = [line.replace(" ", "") for line in data]
+
