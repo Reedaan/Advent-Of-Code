60bf6cc028218efa1f1a597c2881cd0b7ba7b484
Redan <reedann@gmail.com>
2023-12-04 17:32:58 +0100

    Added

--- 2023/Day 1/main.py
+++ 2023/Day 1/main.py
@@ -1,5 +1,4 @@
 import re
-import timeit
 
 data = open("/home/redan/repos/Advent-Of-Code/2023/Day 1/input.txt", "r").readlines()
 data = [line.strip(" ") for line in data]
@@ -53,7 +52,6 @@ def replace_string_with_number(word):
 
 
 def second_part():
-    start = timeit.default_timer()
     sum = 0
     for line in data:
         pattern = re.compile(r"(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))")
@@ -66,37 +64,8 @@ def second_part():
             sum += int(matches[-1])
         else:
             sum += replace_string_with_number(matches[-1]) 
-            
-    stop = timeit.default_timer()
-    print('Time: ', stop - start)
+
     return sum
-
-
-        
-    
-    # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # first_number = 0
-    # second_number = 0
-    # number_list = []
-    # for line in sorted_data:
-    #     for letter in line:
-    #         try:
-    #             if int(letter) in numbers:
-    #                 first_number = letter
-    #                 break
-    #         except ValueError:
-    #             pass
-    #     for letter in line[::-1]:
-    #         try:
-    #             if int(letter) in numbers:
-    #                 second_number = letter
-    #                 break
-    #         except ValueError:
-    #             pass
-    #     number_list.append(first_number + second_number)
-    # number_list_int = [int(x) for x in number_list]
-    
-    # return sum(number_list_int)
 
 # print(first_part())
 print(second_part())
